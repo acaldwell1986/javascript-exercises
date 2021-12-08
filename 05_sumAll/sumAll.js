c2010580b3dbf9433e450cf3bd218c019993dbcf
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-08 15:16:41 -0500

    created sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start, end) {
+    start = 0;
+    let sum = 0;
+       while (start !== end) {
+         start += 1;
+         sum+=start;
+       }
+   return sum;    
+   };
 
 // Do not edit below this line
 module.exports = sumAll;
