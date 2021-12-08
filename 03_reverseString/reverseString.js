b9d86a6e773535be1caa4a9fe2d48f1ce910a0dd
Andrew C <acaldwell1986@gmail.com>
2021-12-08 14:19:44 -0500

    test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
 const reverseString = function(str) {
+    let reverser = [];
+    let reverseStr = '';
+    let count = 0;
 
+    for (let i = str.length; i > 0; i--) {
+        console.log(i);
+        reverser[count] = str.substr(i - 1,1);
+        count++;
+        console.log(reverser);
+    }
+
+    for (let i = 0; i < reverser.length; i++) {
+        reverseStr += reverser[i];
+        console.log(reverseStr);
+    }
+
+    return reverseStr;
 };
 
 // Do not edit below this line
