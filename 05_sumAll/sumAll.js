72e5cf0b6bf8c7bc91f0475a552784560095069f
Andrew C <acaldwell1986@gmail.com>
2021-12-08 15:50:11 -0500

    Passed all test cases for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,24 @@
 const sumAll = function(start, end) {
-    start = 0;
-    let sum = 0;
-       while (start !== end) {
-         start += 1;
-         sum+=start;
-       }
-   return sum;    
-   };
+  let sum = 1;
+  if (start <= 0){
+    return 'ERROR';
+
+  }
+  else if (Number.isInteger(end)) {
+    if (start > end) {
+        let temp = start; 
+        start = end; 
+        end = temp; 
+    };
+    while (start !== end) {
+        start += 1;
+        sum+=start;
+    };
+    return sum; 
+} 
+  else 
+      return 'ERROR' 
+};
 
 // Do not edit below this line
 module.exports = sumAll;
