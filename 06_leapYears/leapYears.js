67a125afa4cfca5352fa7a3723ff03a3af7d12dc
Andrew C <acaldwell1986@gmail.com>
2021-12-09 11:19:25 -0500

    Finished leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+function leapYears(year) {
+    if ((year % 100 === 0) && (year % 400 === 0) && (year % 4 === 0)) {
+        return true;
+    } 
+    else if ((year % 400 === 0) && (year % 100 !== 0)) {
+        return true;   
+    }
+    else if ((year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
