df39d4411cbd4ce4b5010ff2debe0ec21e8bda78
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-17 14:48:25 -0500

    finished exercise 8 and beginning ttd

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,52 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let element of array) {
+    
+    sum += element;
+  }
+  return sum;
 };
 
-const multiply = function() {
 
+const multiply = function(a,b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  const num = a;
+	for (let i = 0; i < b; i++) {
+    a *= num;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
-};
+//10
+
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    for (let i = num - 1; i >= 1; i--) {
+      console.log(num = num * i);
+    }
+  }return num;
+}
+
 
 // Do not edit below this line
-module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+// module.exports = {
+//   add,
+//   subtract,
+//   sum,
+//   multiply,
+//   power,
+//   factorial
+// };
