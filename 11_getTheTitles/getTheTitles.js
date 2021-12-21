83974eedc97dde2031cce5915b86acf702016332
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-21 14:56:35 -0500

    fixed title function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,7 +1,21 @@
+
+const booksExample = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
+
 const getTheTitles = function(arg) {
-    const titles = arg.filter(book => (book.title !== ""));
+    const titles = arg.map(book => `${book.title}`)
     return titles;
 }
 
+
+
 // Do not edit below this line
-module.exports = getTheTitles;
+//module.exports = getTheTitles;
