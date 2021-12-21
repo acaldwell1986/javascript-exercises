d7c3471d00980f68d221afa82a32699b07e345e6
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-21 14:39:23 -0500

    finished fibonacci algorithm

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+    let intArg = parseInt(arg);
+    if (intArg === 1 || 0) {
+        return intArg;
+    }
+    else if (intArg < 0) {
+        return "OOPS"
+    }
+    else {
+        let a = 1; //initial previous value
+        let b = 0;
+        let temp = 0;
+        
+        for (let i = intArg; i > 0; i--) {
+            temp = a; //this loops new prev number for next iteration
+            a = a + b;
+            b = temp;
+        }
+        return b;     
+    }
+    
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
