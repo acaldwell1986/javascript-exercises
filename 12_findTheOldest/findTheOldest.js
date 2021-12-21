399d544f282f6080073c1003f4418fc1e7361f89
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-21 15:43:05 -0500

    troubleshooting

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-};
+const findTheOldest = function(args) {
+   return args.reduce((total, person) => {
+       return total + (person.yearOfBirth);
+   }, 0)
+}
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
-module.exports = findTheOldest;
+//module.exports = findTheOldest;
