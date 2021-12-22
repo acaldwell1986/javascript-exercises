46e2b9c2759d555b72bb25995df541df3c9e82de
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-22 10:48:05 -0500

    prototyping

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,7 +2,7 @@ const people = [
     {
       name: "Carly",
       yearOfBirth: 1942,
-      yearOfDeath: 1970,
+      
     },
     {
       name: "Ray",
@@ -15,14 +15,36 @@ const people = [
       yearOfDeath: 1941,
     },
   ]
-//function returns the sum of all birth years right now
+
+
 const findTheOldest = function(args) {
-   return args.reduce((total, person) => {
-       return total + (person.yearOfBirth);
-   }, 0)
+  args.forEach(element => {
+    if (typeof element.yearOfDeath === 'undefined') {
+      let date = new Date();
+      let year = date.getFullYear();
+      element.age = year - element.yearOfBirth;
+    }
+    else {
+      element.age = element.yearOfDeath - element.yearOfBirth;
+    }
+  });
+  
+  people.sort(function (a,b) {
+    return b.age - a.age;
+  });
+  return args[0];
 }
 
 console.log(findTheOldest(people));
 
+
+
+// Add age property.
+// Loop thru 
+//if death date exists = calculate and add age to property for each name in array
+//else create variable for today's date and subtract birth date from today's date to get age.
+// Sort array by age and return oldest value
+
+
 // Do not edit below this line
-//module.exports = findTheOldest;
+module.exports = findTheOldest;
