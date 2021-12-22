ae79c074b9a3c9de2981749ee069643305f50a55
Andrew C <acaldwell1986@gmail.com>
2021-12-22 10:48:41 -0500

    testing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -16,11 +16,45 @@ const people = [
     },
   ]
 //function returns the sum of all birth years right now
-const findTheOldest = function(args) {
-   return args.reduce((total, person) => {
-       return total + (person.yearOfBirth);
-   }, 0)
-}
+// const findTheOldest = function(array) {
+//   return array.reduce((oldest, currentPerson) => {
+//     const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth
+//     console.log("oldest: " + oldestAge);
+//     const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth
+//     console.log("current: " + currentAge);
+//     return oldestAge < currentAge ? currentPerson : oldest
+//   })
+// };
+
+
+
+
+
+function findTheOldest(array) {
+  let oldestTempA = 0;
+  let oldestTempB = 0;
+  let oldestTempC = 0;
+  for (let i = array.length - 1; i > 0; i--) {
+      console.log(i)
+      
+      oldestTempA = array[i].yearOfDeath - array[i].yearOfBirth;
+      oldestTempB = array[i-1].yearOfDeath - array[i-1].yearOfBirth;
+      console.log(oldestTempB)
+      
+      
+
+    
+  }
+};
+
+
+
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
 console.log(findTheOldest(people));
 
